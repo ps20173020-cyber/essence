a880c5a16be6630cb2c2ae4ef0338da6672ec9f2
ps20173020-cyber <ps20173020@student.vsa.edu.hk>
2026-05-30 10:28:26 +0800

    Add soil moisture sensor loop with servo and LED control

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ def main():
     print("Hello from Essence!")
 
 
+def on_forever():
+    value_of_soil_moisture_0100_at_pin_P1 = 0
+    if value_of_soil_moisture_0100_at_pin_P1 < 50:
+        servos.P0.set_angle(90)
+        pins.digital_write_pin(DigitalPin.P2, 0)
+        basic.show_icon(IconNames.HEART)
+        basic.pause(1000)
+    else:
+        servos.P0.set_angle(0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
+        basic.show_icon(IconNames.NO)
+        basic.pause(1000)
+
+
 if __name__ == "__main__":
     main()
+    basic.forever(on_forever)
