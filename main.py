8f9f4095ae21257be5f9afe37e5f7d132ce5c420
ps20173020-cyber <ps20173020@student.vsa.edu.hk>
2026-05-30 10:56:29 +0800

    Update on_forever function to check soil moisture instead of light level

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ def main():
 
 
 def on_forever():
-    if input.light_level() > 20:
+    value_of_soil_moisture_0100_at_pin_P1 = 0
+    if value_of_soil_moisture_0100_at_pin_P1 < 50:
         servos.P0.set_angle(90)
-        pins.digital_write_pin(DigitalPin.P1, 1)
+        pins.digital_write_pin(DigitalPin.P2, 0)
         basic.show_icon(IconNames.HEART)
         basic.pause(1000)
     else:
         servos.P0.set_angle(0)
-        pins.digital_write_pin(DigitalPin.P1, 0)
+        pins.digital_write_pin(DigitalPin.P2, 1)
         basic.show_icon(IconNames.NO)
         basic.pause(1000)
 
